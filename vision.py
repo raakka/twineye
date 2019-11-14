5ce728266b9a25ecdf6eaacf9866bf7d50d31539
Jacob Rippe <Jacob.Rippe@students.cobbk12.org>
2019-11-14 15:26:20 -0500

    haha you make this in a hurry

--- vision.py
+++ vision.py
@@ -1,6 +1,12 @@
+#imports and stuff
 import cv2
 import numpy as np
 import math import time
+
+#def constants
+stop_calibration = True
+
+
 cap1 = cv2.VideoCapture(0)
 cap2 = cv2.VidepCapture(1)
 
@@ -16,3 +22,14 @@ cap2.set(5,30)
 
 orange_upper_colors = np.array([38,255,255])
 orange_lower_colors = np.array([5,180,125])
+
+
+#calibration for stero values...
+#during calibration target should be 1 meter away from the center 
+
+while stop_calibration == False:
+
+    ret, img1 cap1.read()
+    ret, img2 cap2.read()
+
+    blur1
